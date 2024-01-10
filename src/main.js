e8345de5d1c21cf0ada225eb5802a201b3983b4c
RafiqiAuzan <rafikids32@gmail.com>
2024-01-10 09:06:42 +0700

    install tailwind

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store'; 
 import router from './router.js'; 
+import './index.css'
 
 const app = createApp(App)
 
